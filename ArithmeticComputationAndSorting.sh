6a6d5e470f39a6da9fe29165f4a50445c010bd58
vaishnavipardeshi <vaishnavipardeshi22@gmail.com>
2020-02-11 19:10:56 +0530

    ADDED : First Computation for [a + b * c]

--- ArithmeticComputationAndSorting.sh
+++ ArithmeticComputationAndSorting.sh
@@ -6,3 +6,6 @@ echo "******************************  Welcome To Arithmetic Computation And Sort
 read -p " Enter the number for a : " input1
 read -p " Enter the number for b : " input2
 read -p " Enter the number for c : " input3
+
+# FIRST COMPUTATION FOR [ a + b * c ]
+result1=`echo "scale=2; $input1 + $input2 * $input3" | bc`
