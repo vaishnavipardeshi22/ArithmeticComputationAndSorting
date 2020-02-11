55ed07afa8897a0c33eca6cd1a73027bf7f8ceae
vaishnavipardeshi <vaishnavipardeshi22@gmail.com>
2020-02-11 18:53:48 +0530

    ADDED : Three User Input For a,b,c

--- ArithmeticComputationAndSorting.sh
+++ ArithmeticComputationAndSorting.sh
@@ -1,3 +1,8 @@
 #!/bin/bash -x
 
 echo "******************************  Welcome To Arithmetic Computation And Sorting ******************************"
+
+# GET USER INPUT
+read -p " Enter the number for a : " input1
+read -p " Enter the number for b : " input2
+read -p " Enter the number for c : " input3
