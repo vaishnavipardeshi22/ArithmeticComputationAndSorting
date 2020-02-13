25385fde9e42602dcb25e27a994d7e3a4f7c6df2
vaishnavipardeshi <vaishnavipardeshi22@gmail.com>
2020-02-13 11:05:25 +0530

    ADDED : Sorting Function For Descending Order

--- ArithmeticComputationAndSorting.sh
+++ ArithmeticComputationAndSorting.sh
@@ -2,24 +2,24 @@
 
 echo "******************************  Welcome To Arithmetic Computation And Sorting ******************************"
 
-# GET USER INPUT
+#GET USER INPUT
 read -p " Enter the number for a : " input1
 read -p " Enter the number for b : " input2
 read -p " Enter the number for c : " input3
 
-# FIRST COMPUTATION FOR [ a + b * c ]
+#FIRST COMPUTATION FOR [ a + b * c ]
 result1=`echo "scale=2; $input1 + $input2 * $input3" | bc`
 
-# SECOND COMPUTATION FOR [ a * b + c ]
+#SECOND COMPUTATION FOR [ a * b + c ]
 result2=`echo "scale=2; $input1 * $input2 + $input3" | bc`
 
-# THIRD COMPUTATION FOR [ c + a / b ]
+#THIRD COMPUTATION FOR [ c + a / b ]
 result3=`echo "scale=2; $input3 + $input1 / $input2" | bc`
 
-# FOURTH COMPUTATION FOR [ a % b + c ]
+#FOURTH COMPUTATION FOR [ a % b + c ]
 result4=`echo "$input1 % $input2 + $input3" | bc`
 
-# DECLARE DICTIONARY AND STORE COMPUTATION RESULTS IN DICTIONARY
+#DECLARE DICTIONARY AND STORE COMPUTATION RESULTS IN DICTIONARY
 declare -A resultDictionary
 
 resultDictionary[result1]=$result1
@@ -27,9 +27,29 @@ resultDictionary[result2]=$result2
 resultDictionary[result3]=$result3
 resultDictionary[result4]=$result4
 
-# READ VALUES FROM DICTIONARY IN ARRAY
+#READ VALUES FROM DICTIONARY IN ARRAY
 for (( i=0; i<${#resultDictionary[@]}; i++ ))
 do
 	resultArray[i]=${resultDictionary[result$(( i+1 ))]}
 done
 
+#FUNCTION SORTING RESULTS IN DECENDING ORDER
+function descendingSort()
+{
+	for(( i=0; i<${#resultArray[@]}; i++ ))
+	do
+		for(( j=0; j<${#resultArray[@]}-1; j++ ))
+		do
+			if (( $(echo "${resultArray[j+1]} > ${resultArray[j]}" |bc -l) ))
+			then
+				temp=${resultArray[j]}
+				resultArray[j]=${resultArray[j+1]}
+				resultArray[j+1]=$temp
+			fi
+		done
+	done
+	echo ${resultArray[@]}
+}
+
+#FUNCTION CALL FOR SORTING IN DECENDING ORDER
+descendingSort ${resultArray[@]}
